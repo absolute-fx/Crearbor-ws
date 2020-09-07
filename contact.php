ab8a3959c811c61960ff85d05d7f27b47766392b
Manu <manu@absolute-fx.com>
2020-09-07 17:03:23 +0200

    Debug Fabrice

--- contact.php
+++ contact.php
@@ -1,13 +1,64 @@
-
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
 require './vendor/autoload.php';
 
 use Mailjet\Client;
 use Mailjet\Resources;
-?>
 
+$error = 0;
+
+if (filter_var(htmlentities($_POST['email']), FILTER_VALIDATE_EMAIL)) {
+    $error = 1;
+} else {
+    $fullName = htmlentities($_POST['fullname']);
+    $email = htmlentities($_POST['email']);
+    $mobile = htmlentities($_POST['mobile']);
+    $message = htmlentities($_POST['message']);
+    $mj = new Client('bdb4181cf13a0371af37e7fcb13b3646', '2ac411e58ce996926ade05e28c51e4c3',true,['version' => 'v3.1']);
+
+    $body = [
+        'Messages' => [
+            [
+                'From' => [
+                    'Email' => "philippe.janssens@hotmail.co.uk",
+                    'Name' => "Cre'Arbor"
+                ],
+                'To' => [
+                    [
+                        'Email' => $email,
+                        'Name' => $fullName
+                    ]
+                ],
+                'TemplateID' => 1670943,
+                'TemplateLanguage' => true,
+                'Subject' => "Contact site web Cre'Arbor"
+            ],
+            [
+                'From' => [
+                    'Email' => "philippe.janssens@hotmail.co.uk",
+                    'Name' => "Cre'Arbor"
+                ],
+                'To' => [
+                    [
+                        'Email' => 'philippe.janssens@hotmail.co.uk',
+                        'Name' => "Cre'Arbor"
+                    ]
+                ],
+                'TemplateID' => 1670849,
+                'TemplateLanguage' => true,
+                'Subject' => "Contact site web Cre'Arbor",
+                'Variables' => [
+                    'fullname' => $fullName,
+                    'email' => $email,
+                    'mobile' => $mobile,
+                    'message' => $message
+                ]
+            ]
+        ]
+    ];
+    $response = $mj->post(Resources::$Email, ['body' => $body]);
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,6 +73,8 @@ use Mailjet\Resources;
 
 
 <body>
+
+<?php if ($error == 0): ?>
     <section class="contact-section">
         <div class="container contact-container">
             <div class="">
@@ -30,65 +83,6 @@ use Mailjet\Resources;
                         <img src="./images/logo-white.png" class="img contact-logo" alt="Cre'Arbor logo" id="nav-logo" data-aos="flip-left" data-aos-offset="200" data-aos-duration="800"/>
                     </div>
                 </div>
-                <?php
-                $fullName = $_POST['fullname'];
-                $email = $_POST['email'];
-                $mobile = $_POST['mobile'];
-                $message = $_POST['message'];
-
-                $mj = new Client('bdb4181cf13a0371af37e7fcb13b3646', '2ac411e58ce996926ade05e28c51e4c3',true,['version' => 'v3.1']);
-
-
-                // Define your request body
-
-                $body = [
-                    'Messages' => [
-                        [
-                            'From' => [
-                                'Email' => "manu@absolute-fx.com",
-                                'Name' => "Simon janssens"
-                            ],
-                            'To' => [
-                                [
-                                    'Email' => $email,
-                                    'Name' => $fullName
-                                ]
-                            ],
-                            'TemplateID' => 1670943,
-                            'TemplateLanguage' => true,
-                            'Subject' => "test",
-                            'Variables' => json_decode('{}', true)
-                        ],
-                        [
-                            'From' => [
-                                'Email' => "manu@absolute-fx.com",
-                                'Name' => "Simon janssens"
-                            ],
-                            'To' => [
-                                [
-                                    'Email' => 'Info@absolute-fx.com',
-                                    'Name' => 'Simon janssens'
-                                ]
-                            ],
-                            'TemplateID' => 1670849,
-                            'TemplateLanguage' => true,
-                            'Subject' => "test",
-                            'Variables' => json_decode('{
-                                fullname: $fullName,
-                                email: $email,
-                                mobile: $mobile,
-                                message: $message
-                            }', true)
-                        ]
-                    ]
-                ];
-
-
-                // All resources are located in the Resources class
-
-                $response = $mj->post(Resources::$Email, ['body' => $body]);
-                $response->success() && var_dump($response->getData());
-                ?>
                 <div class="row">
                     <div class="col-md-3"></div>
                     <div class="col-md-6">
@@ -101,9 +95,31 @@ use Mailjet\Resources;
             </div>
         </div>
     </section>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="./js/modernizr.custom.min.js"></script>
-    <script type="text/javascript" src="./js/aos.js"></script>
-    <script type="text/javascript" src="./js/contact.js"></script>
+<?php endif; ?>
+<?php if ($error == 1): ?>
+    <section class="contact-section">
+        <div class="container contact-container">
+            <div class="">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <img src="./images/logo-white.png" class="img contact-logo" alt="Cre'Arbor logo" id="nav-logo" data-aos="flip-left" data-aos-offset="200" data-aos-duration="800"/>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                        <h1 class="crearbor-green-a text-center" data-aos="fade-down">Oups... </h1>
+                        <h2 class="crearbor-brown-a text-center" data-aos="fade-right">Une erreur est survenue, veuillez réessayer plus tard.</h2>
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php endif; ?>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+<script type="text/javascript" src="./js/modernizr.custom.min.js"></script>
+<script type="text/javascript" src="./js/aos.js"></script>
+<script type="text/javascript" src="./js/contact.js"></script>
 </body>
